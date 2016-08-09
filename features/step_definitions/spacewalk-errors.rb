2cb5f2e9200551e6227369f97d85420cc0888f4e
Dario Maiocchi <dmaiocchi@suse.com>
2016-08-09 16:56:05 +0200

    Fix lint-error

--- features/step_definitions/spacewalk-errors.rb
+++ features/step_definitions/spacewalk-errors.rb
@@ -4,8 +4,8 @@
 Then(/^I check the up2date logs on client$/) do
   check_up2date = "grep \"Traceback\" /var/log/up2date"
   _out, _loc, _rem, code = $client.test_and_print_results(check_up2date, "root", 500)
-  if code.zero?
+  if code != 0
     out, _loc, _rem, _code = $client.test_and_print_results("grep -n30 \"Traceback\" /var/log/up2date")
-    raise "an unexepected error found on up2date log : #{out}"
+    raise "TEST_FAILED : ERRORS founds on  up2date log_file !!  : #{out}"
   end
 end
